5651d03d0dfcb775762dd22c9d45428650c2ab8e
iChengbo <chengbo_x@163.com>
2022-01-24 22:02:12 +0800

    feat: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
 export default [
   {
     input: './src/index.js',
     output: [{
-      file: 'dist/index.js',
+      name: 'videojs-xhr-wrapper',
+      file: 'dist/videojs-xhr-wrapper.js',
+      format: 'umd',
+    },{
+      file: 'es/index.js',
       format: 'es'
-    }]
+    }, {
+      file: 'cjs/index.js',
+      format: 'cjs'
+    }],
+    plugins: [
+      resolve(),
+      commonjs()
+    ]
   }
 ]
